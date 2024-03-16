dce94ab3f93daddcaeed3cb6dc747393cd0a7d92
Ahmed Ali <imahmedalibhatti@gmail.com>
2024-03-16 21:50:01 +0000

    feat: add vault system to lock NFT and mint ERC20 tokens

---
+++ src/Vault.sol
@@ -0,0 +1,83 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.24;
+
+import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import {IERC721} from "@openzeppelin/contracts/token/ERC721/IERC721.sol";
+import {ERC721} from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
+import {Token} from "./abstract/Token.sol";
+import {ERC721Holder} from "@openzeppelin/contracts/token/ERC721/utils/ERC721Holder.sol";
+
+contract Vault is Ownable, ERC721Holder, Token {
+    /// @notice The ERC721 token address of the fractional NFT.
+    address public collection;
+
+    /// @notice The ERC721 token ID of the fractional NFT.
+    uint256 public tokenId;
+
+    /// @notice A boolean to indicate if the vault has closed.
+    bool public vaultClosed;
+
+    enum State {
+        inactive,
+        fractionalized,
+        redeemed
+    }
+
+    State public state;
+
+    /// @notice Emitted when an NFT is transferred to the token vault NFT contract.
+    /// @param sender The address that sent the NFT.
+    event DepositedERC721(address indexed sender);
+
+    /// @notice Emitted when a user successfully fractionalizes an NFT and receives the total supply of the newly created ERC20 token.
+    /// @param collection The address of the newly fractionalized NFT.
+    /// @param token The contract address of the newly created ERC20 token.
+    event Fractionalized(address indexed collection, address indexed token);
+
+    /// @notice Emitted when a user successfully redeems an NFT in exchange for the total ERC20 supply.
+    /// @param sender The address that redeemed the NFT (i.e., the address that called redeem()).
+    /// @param collection The address of fractionalized NFT.
+    /// @param tokenId The token Id of fractionalized NFT.
+    event Redeemed(address indexed sender, address indexed collection, uint256 indexed tokenId);
+
+    /// @notice Vault's constructor
+    /// @param _name The desired name of the vault
+    /// @param _symbol The desired symbol of the vault
+    constructor(address _initialOwner, string memory _name, string memory _symbol)
+        Ownable(_initialOwner)
+        Token(_name, _symbol)
+    {
+        state = State.inactive;
+    }
+
+    /// @notice Create a fractionalized NFT: Lock the NFT in the contract; create a new ERC20 token, as specified;
+    ///         and transfer the total supply of the token to the curator.
+    /// @param _to The address of the curator/NFT owner.
+    /// @param _collection The address of the NFT that is to be fractionalized.
+    /// @param _tokenId The token ID of the NFT that is to be fractionalized.
+    /// @param _supply The count of fractions of the fractionalized NFT - the total supply amount of vault ERC20 tokens.
+    /// @dev Note the NFT must be approved for transfer by the owner of NFT token ID.
+    function fractionalize(address _to, address _collection, uint256 _tokenId, uint256 _supply) external onlyOwner {
+        require(state == State.inactive, "State should be inactive");
+        collection = _collection;
+        tokenId = _tokenId;
+        _mint(_to, _supply);
+        state = State.fractionalized;
+
+        emit Fractionalized(collection, address(this));
+    }
+
+    /// @notice A holder of the entire ERC20 supply can call redeem in order to receive the underlying NFT from the contract.
+    ///         The function burns all shares and transfers the vault NFT to the user.
+    /// @dev Note, the ERC20 must be approved for transfer by the TokenVault contract before calling redeem().
+    function redeem() external {
+        uint256 redeemerBalance = IERC20(address(this)).balanceOf(_msgSender());
+        require(redeemerBalance == IERC20(address(this)).totalSupply(), "Redeemer does not hold the entire supply");
+        state = State.redeemed;
+        _burn(_msgSender(), totalSupply());
+        IERC721(collection).safeTransferFrom(address(this), _msgSender(), tokenId);
+
+        emit Redeemed(_msgSender(), collection, tokenId);
+    }
+}
---
+++ src/VaultFactory.sol
@@ -0,0 +1,67 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.24;
+
+import {IERC721} from "@openzeppelin/contracts/token/ERC721/IERC721.sol";
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
+import {Pausable} from "@openzeppelin/contracts/security/Pausable.sol";
+import {CREATE3} from "solmate/utils/CREATE3.sol";
+import {Vault} from "./Vault.sol";
+
+contract VaultFactory is Ownable, Pausable {
+    /// @notice the number of vaults
+    uint256 public vaultCount;
+
+    /// @notice the mapping of vault number to vault contract
+    mapping(uint256 => address) public vaults;
+
+    /// @notice Emitted when a new NFT vault is deployed.
+    event VaultCreated(address collection, uint256 tokenId, address vault, uint256 vaultCount);
+
+    constructor(address _initialOwner) Ownable(_initialOwner) {}
+
+    /// @notice the function to create and deploy a new vault
+    /// @param _collection the ERC721 token address fo the NFT
+    /// @param _tokenId the uint ID of the token
+    /// @param _supply the total supply amount of fractions of the fractionalized NFT
+    /// @param _name the desired name of the vault
+    /// @param _symbol the desired sumbol of the vault
+    /// @return address of the vault
+    function createVault(
+        address _collection,
+        uint256 _tokenId,
+        uint256 _supply,
+        string memory _name,
+        string memory _symbol
+    ) public whenNotPaused onlyOwner returns (address) {
+        bytes32 salt = keccak256(abi.encodePacked(_collection, _tokenId, _supply, _msgSender(), _name, _symbol));
+
+        Vault vault = Vault(
+            CREATE3.deploy(
+                salt, abi.encodePacked(type(Vault).creationCode, abi.encode(_msgSender(), _name, _symbol)), 0
+            )
+        );
+
+        assert(address(vault) == CREATE3.getDeployed(salt));
+
+        IERC721(_collection).transferFrom(_msgSender(), address(vault), _tokenId);
+
+        Vault(vault).fractionalize(_msgSender(), _collection, _tokenId, _supply);
+
+        address vaultAddress = address(vault);
+
+        ++vaultCount;
+        vaults[vaultCount] = vaultAddress;
+
+        emit VaultCreated(_collection, _tokenId, vaultAddress, vaultCount);
+
+        return vaultAddress;
+    }
+
+    function pause() external onlyOwner {
+        _pause();
+    }
+
+    function unpause() external onlyOwner {
+        _unpause();
+    }
+}
